53a17a5675484ed886c017daca586be8ec81dd79
haopham <haophamdv0631@gmail.com>
2025-04-20 21:34:10 +0700

    Merge

--- ebookweb/src/Pages/Home.jsx
+++ ebookweb/src/Pages/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from "../Components/navBar";
 import News from "../Components/news";
 import ReviewList from "../Components/reviewList";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { BookSaleList } from "../Components/BookSaleList";
 import  {BookOnSaleList } from "../Components/BookOnSaleList";
 import { UtilitiesList } from "../Components/UtilitiesList";
@@ -11,6 +12,9 @@ import {RecommendedBookList} from "../Components/RecommendedBookList"
 import {FeaturedProductList} from "../Components/FeaturedProductList"
 import {LogoCard} from "../Components/LogoCard"
 import { BookCarouselHandle } from "../Components/BookCarouselHandle";
+=======
+import ShopDetail from "./shopDetail";
+>>>>>>> 7c30d73 (add2004)
 const HomePage = () => {
     return (
         <>   
